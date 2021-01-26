8b5e66c1400329df226cf551e50df10ec8b230d4
Fede <chiellofederico@gmail.com>
2021-01-26 16:58:50 +0100

    funzione prevImage()

--- js/script.js
+++ js/script.js
@@ -6,15 +6,16 @@ var app = new Vue({
   },
   methods: {
     nextImage(){
-      // if (this.contatore < (this.immagini.length -1)) {
-      //   this.contatore++;
-      // } else {
-      //   this.contatore = 0;
-      // }
-      // console.log(this.contatore);
-
       // Metodo ternario
-      (this.contatore < (this.immagini.length -1)) ? this.contatore++ : this.contatore = 0;
+      // Qui stiamo dicendo che quando il contatore è minore della lunghezza dell'array immagini (4-1=3), cioè contatore < 3, allora incrementa di uno. Altrimenti il contatore = 0.
+      (this.contatore < (this.immagini.length - 1)) ? this.contatore++ : this.contatore = 0;
+      console.log(this.contatore);
+    },
+
+    prevImage(){
+      // Metodo ternario
+      // Qui invece quando il contatore < 1, cioè posizione 0, allora si ritorna all'ultima immagine => contatore = 3. Altrimenti il contatore decrementa di uno.
+      (this.contatore < 1) ? this.contatore = (this.immagini.length -1) : this.contatore--;
       console.log(this.contatore);
     }
 
