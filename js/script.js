13a4512c6e3154eec94b0840cac5445d6fe64798
Fede <chiellofederico@gmail.com>
2021-01-26 17:19:54 +0100

    funzione cliccaPallino()

--- js/script.js
+++ js/script.js
@@ -9,14 +9,17 @@ var app = new Vue({
       // Metodo ternario
       // Qui stiamo dicendo che quando il contatore è minore della lunghezza dell'array immagini (4-1=3), cioè contatore < 3, allora incrementa di uno. Altrimenti il contatore = 0.
       (this.contatore < (this.immagini.length - 1)) ? this.contatore++ : this.contatore = 0;
-      console.log(this.contatore);
+      // console.log(this.contatore);
     },
-
     prevImage(){
       // Metodo ternario
       // Qui invece quando il contatore < 1, cioè posizione 0, allora si ritorna all'ultima immagine => contatore = 3. Altrimenti il contatore decrementa di uno.
       (this.contatore < 1) ? this.contatore = (this.immagini.length -1) : this.contatore--;
-      console.log(this.contatore);
+      // console.log(this.contatore);
+    },
+    cliccaPallino(indice){
+      // Quando il contatore è uguale all'indice, stessa posizione.
+      this.contatore = indice
     }
 
 
